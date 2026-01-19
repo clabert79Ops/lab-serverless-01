39a2bc83324875639eceba17b7e8b0d892767695
clabert79Ops <clabert79.aws@gmail.com>
2026-01-19 21:41:27 +0100

    finish to set CDK

---
+++ stacks/lab_serverless_stack-01.py
@@ -0,0 +1,85 @@
+from aws_cdk import (
+    Stack,
+    CfnParameter,
+    RemovalPolicy,
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    aws_s3 as s3,
+)
+from constructs import Construct
+
+class LabServerlessStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # VPC
+        vpc = ec2.Vpc(
+            self,
+            "LabVPC",
+            cidr="10.0.0.0/16",
+            max_azs=3,
+            nat_gateways=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="public",
+                    subnet_type=ec2.SubnetType.PUBLIC,
+                    cidr_mask=24,
+                ),
+                ec2.SubnetConfiguration(
+                    name="private",
+                    subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidr_mask=24,
+                ),
+            ],
+        )
+
+        # S3 Bucket
+        bucket = s3.Bucket(
+            self,
+            "LabBucket",
+            bucket_name=f"lab-bucket-{self.account}-{self.region}",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        # IAM Role for Lambda
+        lambda_role = iam.Role(
+            self,
+            "LambdaExecutionRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "service-role/AWSLambdaBasicExecutionRole"
+                )
+            ],
+        )
+
+        # Allow Lambda VPC access
+        lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                actions=[
+                    "ec2:CreateNetworkInterface",
+                    "ec2:DescribeNetworkInterfaces",
+                    "ec2:DeleteNetworkInterface",
+                ],
+                resources=["*"],
+            )
+        )
+
+        # Lambda Function
+        event_logger = _lambda.Function(
+            self,
+            "EventLoggerFunction",
+            function_name="eventLogger",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="index.handler",
+            code=_lambda.Code.from_asset("lambda/event_logger"),
+            memory_size=128,
+            timeout=cdk.Duration.seconds(30),
+            role=lambda_role,
+            vpc=vpc,
+            vpc_subnets=ec2.SubnetSelection(
+                subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS
+            ),
+        )
